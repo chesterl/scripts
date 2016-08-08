86839ec5080ca3b26e43086a29e693078e2269c6
Chester Lam <chesterlam620@gmail.com>
2016-08-09 09:20:55 +1000

    First commit - TMUX work scripts

---
+++ tmuxofwork.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+tmux new -s WORK -n CONSOLE -d
+tmux send-keys -t WORK 'cd ~/oneflare/site' C-m
+tmux send-keys -t WORK 'rails c' C-m
+
+tmux new-window -n RSPEC -t WORK
+tmux send-keys -t WORK:2 'cd ~/oneflare/site' C-m
+tmux send-keys -t WORK:2 'guard' C-m
+
+tmux new-window -n SITE -t WORK
+tmux send-keys -t WORK:3 'cd ~/oneflare/site' C-m
+tmux send-keys -t WORK:3 'vim' C-m
+
+tmux new-window -n Bash -t WORK
+tmux send-keys -t WORK:4 'cd ~/oneflare/site' C-m
+tmux select-window -t WORK:1 
+tmux attach -t WORK
+
+
