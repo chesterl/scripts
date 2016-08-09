b8b487f92dda2a5715cf0956cf5ed8972b3a1c37
Chester Lam <chesterlam620@gmail.com>
2016-08-10 09:56:27 +1000

    Tmux WOM config

---
+++ tmuxwom.sh
@@ -0,0 +1,19 @@
+tmux new -s WOM -n CONSOLE -d
+tmux send-keys -t WOM 'cd ~/oneflare/womo' C-m
+tmux send-keys -t WOM 'rails c' C-m
+
+tmux new-window -n RSPEC -t WOM
+tmux send-keys -t WOM:2 'cd ~/oneflare/womo' C-m
+tmux send-keys -t WOM:2 'guard' C-m
+
+tmux new-window -n SITE -t WOM
+tmux send-keys -t WOM:3 'cd ~/oneflare/womo' C-m
+tmux send-keys -t WOM:3 'vim' C-m
+
+tmux new-window -n SERVER -t WOM
+tmux send-keys -t WOM:4 'cd ~/oneflare/womo' C-m
+tmux send-keys -t WOM:4 'rails s' C-m
+tmux select-window -t WOM:3 
+tmux attach -t WOM
+
+
