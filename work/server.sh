69d5269fa3c788f38e89e1644462d173f16c6bab
Chester Lam <chesterlam620@gmail.com>
2017-04-02 22:44:33 +1000

    Server adjustments

--- work/server.sh
+++ work/server.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 tmux new -s SERVER -n REDIS -d
+# tmux send-keys -t SERVER 'mysql.server start' C-m
 tmux send-keys -t SERVER 'cd ~/oneflare/site' C-m
 tmux send-keys -t SERVER 'redis-server' C-m
 
-tmux new-window -n Elastic -t SERVER
-tmux send-keys -t SERVER:2 'cd ~/oneflare/site' C-m
-tmux send-keys -t SERVER:2 'elasticsearch' C-m
+# tmux new-window -n Elastic -t SERVER
+# tmux send-keys -t SERVER:2 'cd ~/oneflare/site' C-m
+# tmux send-keys -t SERVER:2 'elasticsearch' C-m
 
 tmux new-window -n SERVER -t SERVER
-tmux send-keys -t SERVER:3 'cd ~/oneflare/site' C-m
-tmux send-keys -t SERVER:3 'rails s' C-m
+tmux send-keys -t SERVER:2 'cd ~/oneflare/site' C-m
+tmux send-keys -t SERVER:2 'rails s' C-m
 
 tmux select-window -t SERVER:3
 tmux attach -t SERVER
